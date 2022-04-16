8f49c06b9c5d7e125632160d5d309d221a0baf66
Ovi CT <catarama.ovidiu66@gmail.com>
2022-04-17 02:41:07 +0300

    update index.html

--- client/js/home.js
+++ client/js/home.js
@@ -1,8 +1,21 @@
 document.body.addEventListener('scroll', () => {
-    var x = document.getElementById("homeContent");
+    var x = document.getElementById("homeContent1");
 
-    var left = document.getElementById("homeLeft");
-    var right = document.getElementById("homeRight");
+    var left = document.getElementById("homeLeft1");
+    var right = document.getElementById("homeRight1");
+ 
+    if(document.body.scrollTop >= x.offsetTop - 500)
+    {
+        left.style.transform = "TranslateX(0)";
+        right.style.transform = "TranslateX(0)";
+    }
+}, false);
+
+document.body.addEventListener('scroll', () => {
+    var x = document.getElementById("homeContent2");
+
+    var left = document.getElementById("homeLeft2");
+    var right = document.getElementById("homeRight2");
  
     if(document.body.scrollTop >= x.offsetTop - 500)
     {
